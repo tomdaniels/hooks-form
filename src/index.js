36215bb294d76e4c540d7b4799bb2153726320bc
tomdaniels <thomsondaniels@hotmail.com>
2019-04-13 16:08:09 +1000

    fix import

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Form from 'form.js';
+import Form from './form.js';
 
 ReactDOM.render(<Form />, document.getElementById('root'));
